672ee5ecae0960507d4fafd7cca860a9974a2f0a
Ryan Kannegiesser <rkannegiesser@gmail.com>
2014-04-07 10:28:43 -0500

    add .my.cnf

--- symlinker.sh
+++ symlinker.sh
@@ -9,7 +9,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="ackrc bash_profile bashrc gemrc gitconfig gitignore inputrc irbrc rvmrc vimrc" # list of files/folders to symlink in homedir
+files="ackrc bash_profile bashrc gemrc gitconfig gitignore inputrc irbrc rvmrc vimrc my.cnf" # list of files/folders to symlink in homedir
 
 ##########
 
@@ -26,7 +26,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file ~/.dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
